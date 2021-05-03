5dbb72a09daf9db95ab6c0838dc7526bf8e22122
Spencerp34 <spencerp34@yahoo.com>
2021-05-03 11:13:13 -0600

    did top content. Just trying to fix my main content..

--- js/index.js
+++ js/index.js
@@ -40,3 +40,55 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//Nav Bar
+
+const NavBar1 = document.querySelector('nav a:nth-of-type(1)');
+NavBar1.textContent = NavBar1.textContent = siteContent.nav['nav-item-1'];
+const NavBar2 = NavBar1.nextElementSibling;
+NavBar2.textContent = NavBar1.textContent = siteContent.nav['nav-item-2'];
+const NavBar3 = NavBar2.nextElementSibling;
+NavBar3.textContent = NavBar1.textContent = siteContent.nav['nav-item-3'];
+const NavBar4 = NavBar3.nextElementSibling;
+NavBar4.textContent = NavBar1.textContent = siteContent.nav['nav-item-4'];
+const NavBar5 = NavBar4.nextElementSibling;
+NavBar5.textContent = NavBar1.textContent = siteContent.nav['nav-item-5'];
+const NavBar6 = NavBar5.nextElementSibling;
+NavBar6.textContent = NavBar1.textContent = siteContent.nav['nav-item-6'];
+
+// Top Content
+
+const cta = siteContent["cta"]["h1"].split(' ');
+const title = document.getElementsByTagName('h1')[0];
+title.innerHTML = cta.join('<br>');
+
+const ctaButton = document.querySelector('.cta-text button');
+ctaButton.textContent = siteContent.cta['button'];
+
+const ctaImg = document.querySelector('#cta-img');
+ctaImg.src = siteContent.cta['img-src']
+
+// Main content
+
+const features = document.querySelector('.text-content h4:nth-of-type(1)');
+const featuresText = document.querySelector('.text-content p:nth-of-type(1)');
+
+
+const about = features.nextElementSibling;
+const services = about.nextElementSibling;
+const product = services.nextElementSibling;
+const vision = product.nextElementSibling;
+
+const aboutText = featuresText.nextElementSibling;
+const servicesText = aboutText.nextElementSibling;
+const productText = servicesText.nextElementSibling;
+const visionText = productText.nextElementSibling;
+
+
+contentTitles.textContent = siteContent["main-content"]['features-h4'];
+contentTitles2.textContent = 'Yolo';
+ siteContent["main-content"]['about-h4'];
+
+
+contentText.textContent = siteContent['main-content']['features-content']
+
